a12ec5121e20ccdf8f3343d162d93a7dd22dd6bd
higaki-tatsuro <higaki8225@icloud.com>
2024-05-06 19:11:31 +0900

    2024/5/6 進捗

--- src/main/java/guru/springframework/msscbeerservice/MsscBeerServiceApplication.java
+++ src/main/java/guru/springframework/msscbeerservice/MsscBeerServiceApplication.java
@@ -3,8 +3,10 @@ package guru.springframework.msscbeerservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 
+@EnableFeignClients
 @SpringBootApplication
 public class MsscBeerServiceApplication {
 
